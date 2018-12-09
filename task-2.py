f4ca28509bb3d23eb64f3131aeeae09319800d4a
Matsko A <AMatsko54@mail.ru>
2018-12-09 22:15:55 +0700

    task-1 works task-2 ready for 50% task-3 not started

--- task-2.py
+++ task-2.py
@@ -22,31 +22,64 @@ print('Загадано четырехбуквенное слово из бук�
 print('Отгадай!')
 s = 0
 d = 0
+guessed = 'ЕНОТ'
 while n <= 10 and s != 4:
     print('Попытка №: ', n)
     answer = input().upper()
     if answer[0] == guessed[0]:
         s = s + 1
-    elif answer[0] != guessed[0] and guessed.find(answer[0]) != -1:
-        d = d + 1
+    elif answer[0] != guessed[0]:
+        if guessed.find(answer[0]) != -1:
+            num_letter1 = answer.count(answer[0])
+            num_letter2 = guessed.count(answer[0])
+            if num_letter1 == num_letter2:
+                d = d + num_letter1
+            elif num_letter1 > num_letter2:
+                d = d + num_letter2
+            elif num_letter1 < num_letter2:
+                d = d + num_letter1
     else:
         pass
     if answer[1] == guessed[1]:
         s = s + 1
-    elif answer[1] != guessed[1] and guessed.find(answer[1]) != -1:
-        d = d + 1
+    elif answer[1] != guessed[1]:
+        if guessed.find(answer[1]) != -1:
+            num_letter1 = answer.count(answer[1])
+            num_letter2 = guessed.count(answer[1])
+            if num_letter1 == num_letter2:
+                d = d + num_letter1
+            elif num_letter1 > num_letter2:
+                d = d + num_letter2
+            elif num_letter1 < num_letter2:
+                d = d + num_letter1
     else:
         pass
     if answer[2] == guessed[2]:
         s = s + 1
-    elif answer[2] != guessed[2] and guessed.find(answer[2]) != -1:
-        d = d + 1
+    elif answer[2] != guessed[2]:
+        if guessed.find(answer[2]) != -1:
+            num_letter1 = answer.count(answer[2])
+            num_letter2 = guessed.count(answer[2])
+            if num_letter1 == num_letter2:
+                d = d + num_letter1
+            elif num_letter1 > num_letter2:
+                d = d + num_letter2
+            elif num_letter1 < num_letter2:
+                d = d + num_letter1
     else:
         pass
     if answer[3] == guessed[3]:
         s = s + 1
-    elif answer[3] != guessed[3] and guessed.find(answer[3]) != -1:
-        d = d + 1
+    elif answer[3] != guessed[3]:
+        if guessed.find(answer[3]) != -1:
+            num_letter1 = answer.count(answer[3])
+            num_letter2 = guessed.count(answer[3])
+            if num_letter1 == num_letter2:
+                d = d + num_letter1
+            elif num_letter1 > num_letter2:
+                d = d + num_letter2
+            elif num_letter1 < num_letter2:
+                d = d + num_letter1
     else:
         pass
     if s == 4:
@@ -54,8 +87,8 @@ while n <= 10 and s != 4:
     else:
         print('На \"своём месте\": ', s)
         print('На \"чужом месте\": ', d)
-    s = 0
-    d = 0
+        s = 0
+        d = 0
     n = n + 1
     if n == 11 and s != 4:
         print('Вы проиграли!')
